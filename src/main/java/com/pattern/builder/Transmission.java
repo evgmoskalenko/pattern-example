002c0b9abc624066dd30db10f851ad909fdf35c7
Evgeniy Moskalenko <qa@dizboard.com>
2017-06-16 19:55:36 +0300

    Add builder pattern

---
+++ src/main/java/com/pattern/builder/Transmission.java
@@ -0,0 +1,8 @@
+package com.pattern.builder;
+
+/**
+ * Одна из фишек автомобиля.
+ */
+public enum Transmission {
+    SINGLE_SPEED, MANUAL, AUTOMATIC, SEMI_AUTOMATIC;
+}
---
+++ src/main/java/com/pattern/builder/Type.java
@@ -0,0 +1,6 @@
+package com.pattern.builder;
+
+
+public enum Type {
+    CITY_CAR, SPORTS_CAR, SUV;
+}
